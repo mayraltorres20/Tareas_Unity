d6e25788c8c79e1261f2011f50bb5bf0159126a8
mayraltorres20 <66183694+mayraltorres20@users.noreply.github.com>
2020-10-27 18:43:57 -0600

    primera catualización

---
+++ unity1.1/Assets/scripts/Camara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    //public int puntaje = 350;
+    //public int tiempo = 40;
+    public float gravedad = 9.81f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // float ptsSeg = (float) puntaje / tiempo; //cast <-conversion de datos
+        // Debug.Log("Puntos por Segundo");
+        // Debug.Log(ptsSeg);
+        Debug.Log(gravedad);
+    }
+
+   
+}
