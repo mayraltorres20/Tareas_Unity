d9aca22d5aecc1e192a56067a5bda399122c7c93
mayraltorres20 <66183694+mayraltorres20@users.noreply.github.com>
2020-11-02 21:53:46 -0600

    Agrega el movimiento al personaje

--- unity1.1/Assets/scripts/Ariel.cs
+++ unity1.1/Assets/scripts/Ariel.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Ariel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        //Rigidbody2D MovDer = GetComponent<Rigidbody2D>();
+        //MovDer.AddForce(new Vector2(-1,0), ForceMode2D.Impulse);
+        const float ImpulsoMinimo = 3f;
+        const float ImpulsoMaximo = 5f;
+
+        float angulo = Random.Range(0, 2 * Mathf.PI);
+
+        Vector2 direccion = new Vector2(Mathf.Cos(angulo), Mathf.Sin(angulo));
+        float magnitud = Random.Range(ImpulsoMinimo, ImpulsoMaximo);
         Rigidbody2D MovDer = GetComponent<Rigidbody2D>();
-        MovDer.AddForce(new Vector2(1,0), ForceMode2D.Impulse);
+        MovDer.AddForce(direccion*magnitud, ForceMode2D.Impulse);
+
+        Debug.Log(magnitud);
+
+        
     }
+
 
     // Update is called once per frame
     void Update()
